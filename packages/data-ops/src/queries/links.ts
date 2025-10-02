f5803f89c7d21283b0273618cadfab352438511c
Zephyrrr <kuangdadu@gmail.com>
2025-10-02 16:33:01 -0700

    Refactor TRPC routes for link management to utilize new database queries. Implement getLinks and getLink functions for improved data retrieval, and update link name and destinations functionalities.

--- packages/data-ops/src/queries/links.ts
+++ packages/data-ops/src/queries/links.ts
@@ -1,7 +1,13 @@
 import { getDb } from "@/db/database";
-import { CreateLinkSchemaType } from "@/zod/links";
-import { nanoid } from "nanoid";
 import { links } from "@/drizzle-out/schema";
+import {
+  CreateLinkSchemaType,
+  destinationsSchema,
+  DestinationsSchemaType,
+  linkSchema,
+} from "@/zod/links";
+import { and, desc, eq, lt } from "drizzle-orm";
+import { nanoid } from "nanoid";
 
 export async function createLink(
   data: CreateLinkSchemaType & { accountId: string }
@@ -15,4 +21,83 @@ export async function createLink(
     destinations: JSON.stringify(data.destinations),
   });
   return id;
+}
+
+export async function getLinks(accoundid: string, createdBefore?: string) {
+  const db = getDb();
+
+  const conditions = [eq(links.accountId, accoundid)];
+
+  if (createdBefore) {
+    conditions.push(lt(links.created, createdBefore));
+  }
+
+  const result = await db
+    .select({
+      linkId: links.linkId,
+      destinations: links.destinations,
+      created: links.created,
+      name: links.name,
+    })
+    .from(links)
+    .where(and(...conditions))
+    .orderBy(desc(links.created))
+    .limit(25);
+
+  return result.map((link) => ({
+    ...link,
+    lastSixHours: Array.from({ length: 6 }, () =>
+      Math.floor(Math.random() * 100)
+    ),
+    linkClicks: 6,
+    destinations: Object.keys(JSON.parse(link.destinations)).length,
+  }));
+}
+
+export async function getLink(linkId: string) {
+  const db = getDb();
+
+  const result = await db
+    .select()
+    .from(links)
+    .where(eq(links.linkId, linkId))
+    .limit(1);
+
+  if (!result.length) {
+    return null;
+  }
+
+  const link = result[0];
+  const parsedLink = linkSchema.safeParse(link);
+  if (!parsedLink.success) {
+    console.log(parsedLink.error);
+    throw new Error("BAD_REQUEST Error Parsing Link");
+  }
+  return parsedLink.data;
+}
+
+export async function updateLinkDestinations(
+  linkId: string,
+  destinations: DestinationsSchemaType
+) {
+  const destinationsParsed = destinationsSchema.parse(destinations);
+  const db = getDb();
+  await db
+    .update(links)
+    .set({
+      destinations: JSON.stringify(destinationsParsed),
+      updated: new Date().toISOString(),
+    })
+    .where(eq(links.linkId, linkId));
+}
+
+export async function updateLinkName(linkId: string, name: string) {
+  const db = getDb();
+  await db
+    .update(links)
+    .set({
+      name,
+      updated: new Date().toISOString(),
+    })
+    .where(eq(links.linkId, linkId));
 }
