7cb35d148d2a59b2d3f8f036cc7931db592aac0d
Zephyrrr <kuangdadu@gmail.com>
2025-10-06 20:52:19 -0700

    Implement message handling for LINK_CLICK events in DataService and add link click processing logic. Introduce new queue handler for link clicks and update database queries to support link click data insertion.

--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -1,6 +1,8 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
 import { App } from './hono/app';
 import { initDatabase } from '@repo/data-ops/database';
+import { QueueMessageSchema } from '@repo/data-ops/zod-schema/queue';
+import { handleLinkClick } from './queue-handlers/link-clicks';
 
 export default class DataService extends WorkerEntrypoint<Env> {
 	constructor(ctx: ExecutionContext, env: Env) {
@@ -12,7 +14,20 @@ export default class DataService extends WorkerEntrypoint<Env> {
 	}
 	async queue(batch: MessageBatch<unknown>): Promise<void> {
 		for (const message of batch.messages) {
-			console.log('Queue Event Received:', message.body);
+			const parsedEvent = QueueMessageSchema.safeParse(message.body);
+			if (parsedEvent.success) {
+				const event = parsedEvent.data;
+				switch (event.type) {
+					case 'LINK_CLICK':
+						await handleLinkClick(this.env, event);
+						break;
+					default:
+						console.error(`Unknown event type: ${event.type}`);
+						break;
+				}
+			} else {
+				console.error(parsedEvent.error);
+			}
 		}
 	}
 }
